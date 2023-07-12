103cce2eb110d893178a681321e5aeae4785ea17
Kevin Osei <knokwaku@gmail.com>
2023-07-12 14:41:07 +0000

    worked on upload image section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './index.css'
 
 
 const App = () => (
-  <div className='flex borde border-blue-400 h-[500px] justify-between font-family:Jost'>
-    <div className='borde border-blue-400 w-[900px] text-center self-center'>
+  <div className='flex border border-blue-400 h-[500px] justify-between font-family:Jost'>
+    <div className='justify-center grid justify-items-center border border-blue-400 w-[900px]  '>
       <h1 className='text-6xl font-bold underline decoration-yellow-300 decoration-[15px]'>Face Detection Engine</h1>
-      <p className='text-2xl mt-[50px]'>UPLOAD PHOTO AND DETECT FACES </p>
+      <p className='text-2xl mt-[0px]'>UPLOAD PHOTO AND DETECT FACES </p>
+      <div className='flex rounded-full border border-black mt-[0px] w-[600px] h-[60px] justify-around '>
+        <p className='text-lg'>Upload a Photo</p>
+        <img src='./camera.png' width='50px' height='50px' />
+      </div>
     </div>
-    <div className='flex borde border-blue-400'>
+    <div className='flex border border-blue-400'>
       <img src='./facemesh.png' width='350px' height='00px' />
     </div>
   </div>
