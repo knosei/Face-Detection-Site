6298bd73274e488b182ef57fd0aedf26c0ded675
Kevin Osei <knokwaku@gmail.com>
2023-07-17 20:24:16 +0000

    created about section

--- src/About.jsx
+++ src/About.jsx
@@ -5,11 +5,19 @@ const About = () => {
     <div className='flex bg-blue-600 justify-center items-center'>
         <div className='flex border border-blue-00 h-[700px] w-[1078px] justify-around items-center'>
             <div className='flex flex-col justify-between h-[558px]'>
-                <div className='border h-[240px] w-[685px] rounded-lg'></div>
-                <div className='border h-[270px] w-[685px] rounded-lg'></div>
+                <div className='border h-[240px] w-[685px] rounded-lg bg-white text-black'>
+                    <h4>What is a reverse image search?</h4>
+                    <p>A reverse image search is a technique that allows finding things, people, brands, etc. using a photo. While performing a regular search you usually type a word or phrase that is related to the information you are trying to find; when you do a reverse image search, you upload a picture to a search engine. In the results of regular searches, you receive a list of websites that are connected to these phrases. When you perform a reverse image search, in the results you receive photos of similar things, people, etc, linked to websites about them. Reverse search by image is the best solution to use when looking for similar images, smaller/bigger versions of them, or twin content. The best-known reverse image search engine is Google Images.</p>
+                </div>
+                <div className='border h-[270px] w-[685px] rounded-lg bg-white text-black'>
+                    <h4></h4>
+                    <p></p>
+                </div>
             </div>
-            <div className='border h-[558px] w-[333px]'></div>
-        
+            <div className='border h-[558px] w-[333px] rounded-lg bg-white text-black'>
+                <h4></h4>
+                <p></p>
+            </div>
         </div>
     </div>
     
